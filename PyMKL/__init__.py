1492c47e1630b35d8dff3133aa3b314e93914366
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2022-05-06 18:32:30 +0200

    .

--- PyMKL/__init__.py
+++ PyMKL/__init__.py
@@ -1,3 +1,3 @@
 from .__ops import MKL
 
-__version__ = "0.0.0.2"
+__version__ = "0.0.0.3"
--- setup.py
+++ setup.py
@@ -33,19 +33,6 @@ def get_version(rel_path):
     else:
         raise RuntimeError("Unable to find version string.")
 
-# Compile C++ code
-setup(
-    py_modules = ["libPyMKL"],
-    ext_modules=[
-        Extension(
-            "libPyMKL",
-            ["PyMKL/lib/libPyMKL.c",],
-        ),
-    ],
-    cmdclass={'build_ext': build_ext},
-)
-
-
 # Generate Python library
 setup(
     name="PyMKL",
@@ -68,4 +55,12 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    py_modules = ["libPyMKL"],
+    ext_modules=[
+        Extension(
+            "libPyMKL",
+            ["PyMKL/lib/libPyMKL.c",],
+        ),
+    ],
+    cmdclass={'build_ext': build_ext},
 )
