6f2abc47e8222837462e8be54438aa0a7ef71877
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2022-05-19 20:21:37 +0200

    .

--- PyMKL/__ops.py
+++ PyMKL/__ops.py
@@ -55,13 +55,13 @@ class MKL():
             warnings.warn("BEWARE! Algorithm works much better with the 'smcp' solver, installed via 'pip install smcp', but some systems fail when installing it")
         if self.lib.lower() not in ["cpp", "numba"]:
             self.lib    = "cpp"
-            self.__lib  = PyMKL.lib.cpp
+            self.lib_fcn= PyMKL.lib.cpp
             warnings.warn("Selecting C++ as default")
         if (self.lib.lower() == "numba") or not self.flagCpp:
             if (self.lib.lower() == "cpp") and not self.flagCpp:
                 warnings.warn("ERROR! C++ not imported correctly. Falling back to numba")
             self.lib    = "numba"
-            self.__lib  = PyMKL.lib.numba
+            self.lib_fcn= PyMKL.lib.numba
             self.flagCpp= False
 
         # Break symmetry
@@ -86,17 +86,17 @@ class MKL():
                 SW_betas            = np.zeros((self.N, self.N))
                 SD_betas            = np.zeros((self.N, self.N))
 
-                ParallelResult      = Parallel(n_jobs=numWorkers, prefer="threads")(delayed(self.__lib.computeSWB)(np.moveaxis(self.K,0,-1),self.betas,self.W,np.diag(self.D).copy(),start_values[i],end_values[i]) for i in range(len(start_values)))    
+                ParallelResult      = Parallel(n_jobs=numWorkers, prefer="threads")(delayed(self.lib_fcn.computeSWB)(np.moveaxis(self.K,0,-1),self.betas,self.W,np.diag(self.D).copy(),start_values[i],end_values[i]) for i in range(len(start_values)))    
 
                 for (SW_betastmp,SD_betastmp) in ParallelResult:
                     SW_betas        = SW_betas + SW_betastmp
                     SD_betas        = SD_betas + SD_betastmp
             except NameError:
-                (SW_betas,SD_betas) = self.__lib.computeSWB(np.moveaxis(self.K,0,-1),self.betas,self.W,np.diag(self.D).copy(),0,self.N)
+                (SW_betas,SD_betas) = self.lib_fcn.computeSWB(np.moveaxis(self.K,0,-1),self.betas,self.W,np.diag(self.D).copy(),0,self.N)
             except KeyboardInterrupt:
                 raise
         else:
-            (SW_betas,SD_betas)     = self.__lib.computeSWB(np.moveaxis(self.K,0,-1),self.betas,self.W,np.diag(self.D).copy(),0,self.N)
+            (SW_betas,SD_betas)     = self.lib_fcn.computeSWB(np.moveaxis(self.K,0,-1),self.betas,self.W,np.diag(self.D).copy(),0,self.N)
 
         SW_betas            = SW_betas + np.triu(SW_betas,1).T
         SD_betas            = SD_betas + np.triu(SD_betas,1).T
@@ -140,17 +140,17 @@ class MKL():
                 SW_A            = np.zeros((self.M, self.M))
                 SD_A            = np.zeros((self.M, self.M))
 
-                ParallelResult  = Parallel(n_jobs=numWorkers, prefer="threads")(delayed(self.__lib.computeSWA)(np.moveaxis(self.K,0,-1),self.A,self.W,np.diag(self.D).copy(),start_values[i],end_values[i]) for i in range(len(start_values)))    
+                ParallelResult  = Parallel(n_jobs=numWorkers, prefer="threads")(delayed(self.lib_fcn.computeSWA)(np.moveaxis(self.K,0,-1),self.A,self.W,np.diag(self.D).copy(),start_values[i],end_values[i]) for i in range(len(start_values)))    
 
                 for (SW_Atmp,SD_Atmp) in ParallelResult:
                     SW_A        = SW_A + SW_Atmp
                     SD_A        = SD_A + SD_Atmp
             except NameError:
-                (SW_A,SD_A)     = self.__lib.computeSWA(np.moveaxis(self.K,0,-1),self.A,self.W,np.diag(self.D).copy(),0,self.N)
+                (SW_A,SD_A)     = self.lib_fcn.computeSWA(np.moveaxis(self.K,0,-1),self.A,self.W,np.diag(self.D).copy(),0,self.N)
             except KeyboardInterrupt:
                 raise
         else:
-            (SW_A,SD_A)         = self.__lib.computeSWA(np.moveaxis(self.K,0,-1),self.A,self.W,np.diag(self.D).copy(),0,self.N)
+            (SW_A,SD_A)         = self.lib_fcn.computeSWA(np.moveaxis(self.K,0,-1),self.A,self.W,np.diag(self.D).copy(),0,self.N)
 
         # Solve solution throwing double the result in C compiled code
         SW_A                = (SW_A + np.triu(SW_A,1).T)
@@ -190,7 +190,7 @@ class MKL():
                 gap             = 0
                 constr          = 0
 
-                ParallelResult = Parallel(n_jobs=numWorkers, prefer="threads")(delayed(self.__lib.computeENERGY)(np.moveaxis(self.K,0,-1), self.betas, self.A, self.W, np.diag(self.D).copy(), start_values[i],end_values[i]) for i in range(len(start_values)))
+                ParallelResult = Parallel(n_jobs=numWorkers, prefer="threads")(delayed(self.lib_fcn.computeENERGY)(np.moveaxis(self.K,0,-1), self.betas, self.A, self.W, np.diag(self.D).copy(), start_values[i],end_values[i]) for i in range(len(start_values)))
 
                 for (gaptmp,constrtmp) in ParallelResult:
                     gap         = gap    + gaptmp
@@ -199,11 +199,11 @@ class MKL():
                 gap             = gap
                 constr          = constr
             except NameError:
-                (gap, constr)   = self.__lib.computeENERGY(np.moveaxis(self.K,0,-1), self.betas, self.A, self.W, np.diag(self.D).copy(), 0, self.N)
+                (gap, constr)   = self.lib_fcn.computeENERGY(np.moveaxis(self.K,0,-1), self.betas, self.A, self.W, np.diag(self.D).copy(), 0, self.N)
             except KeyboardInterrupt:
                 raise
         else:
-            (gap, constr)       = self.__lib.computeENERGY(np.moveaxis(self.K,0,-1), self.betas, self.A, self.W, np.diag(self.D).copy(), 0, self.N)
+            (gap, constr)       = self.lib_fcn.computeENERGY(np.moveaxis(self.K,0,-1), self.betas, self.A, self.W, np.diag(self.D).copy(), 0, self.N)
 
         # Store energy
         self.energy.append(gap)
