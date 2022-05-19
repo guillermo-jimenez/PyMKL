2507ff783257c9028ba40bd2e5ac9db2c6152dd2
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2022-05-19 20:14:28 +0200

    Import cpp and numba as modules

--- PyMKL/lib/__init__.py
+++ PyMKL/lib/__init__.py
@@ -1,5 +1,8 @@
 import os
+PyMKL.lib.cpp = None
+import PyMKL.lib.numba
 try:
+    import PyMKL.lib.cpp
     from PyMKL.lib.cpp import computeENERGY
     from PyMKL.lib.cpp import computeSWA
     from PyMKL.lib.cpp import computeSWB
